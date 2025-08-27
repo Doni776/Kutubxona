a3a23291f2ef14b74018ae13fd126d550da05728
Doniyor <donyor6hamidov25@gmail.com>
2025-08-27 15:01:09 +0500

    models finished

---
+++ main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Talaba,Muallif,Kitob,Admin,Record
+
+admin.site.register(Talaba)
+admin.site.register(Muallif)
+admin.site.register(Kitob)
+admin.site.register(Admin)
+admin.site.register(Record)
